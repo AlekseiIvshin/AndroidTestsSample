3fd406315cecb7b7fe74b6da74ba375e44854d22
Aleksei Ivshin <Aleksei_Ivshin@epam.com>
2015-11-20 10:05:44 +0400

    Sample 1: green test

--- app/src/main/java/com/alekseiivhsin/samples/testedproject/MainActivity.java
+++ app/src/main/java/com/alekseiivhsin/samples/testedproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.alekseiivhsin.samples.testedproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        findViewById(R.id.show_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivityIntent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(nextActivityIntent);
             }
         });
     }
